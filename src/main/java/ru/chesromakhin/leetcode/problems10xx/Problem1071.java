6d2e79b5c2a2a038bb63e7f6eca849e991aab96b
Ches Romakhin <chesromakhin@gmail.com>
2023-02-02 11:51:11 +0100

    Optimize solution for problem 1071.

--- src/main/java/ru/chesromakhin/leetcode/problems10xx/Problem1071.java
+++ src/main/java/ru/chesromakhin/leetcode/problems10xx/Problem1071.java
@@ -11,10 +11,8 @@ public class Problem1071 {
 
     for (int length = str2.length(); length > 0; length--) {
       if (str2.length() % length == 0 && str1.length() % length == 0) {
-        String div = str2.substring(0, length);
-
-        if (isDivisor(str1, str2, div)) {
-          return div;
+        if (isDivisor(str1, str2, length)) {
+          return str2.substring(0, length);
         }
       }
     }
@@ -22,12 +20,12 @@ public class Problem1071 {
     return "";
   }
 
-  private boolean isDivisor(String str1, String str2, String div) {
+  private boolean isDivisor(String str1, String str2, int length) {
     for (int i = 0; i < str1.length(); i++) {
-      int divI = i % div.length();
+      int divI = i % length;
 
-      if ((div.charAt(divI) != str1.charAt(i)) ||
-        (str2.length() > i && div.charAt(divI) != str2.charAt(i))) {
+      if ((str2.charAt(divI) != str1.charAt(i)) ||
+        (str2.length() > i && str2.charAt(divI) != str2.charAt(i))) {
         return false;
       }
     }
